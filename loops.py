a2d84486115ae90c2124d6764962ebe553bbf004
fridahnankwanga <fridahnankwanga@gmail.com>
2025-07-02 12:39:30 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,22 @@
+"""
+### Syntax
+for count in condition:
+    logic
+"""
+''' 
+Write a program that prints the first 100 numbers
+'''
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+print("...")
+print("100")
+for count in range(1, 101):
+    print(count)
